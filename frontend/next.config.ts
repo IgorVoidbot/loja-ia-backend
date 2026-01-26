1c1577e452dbebc423d21e7324fff7fb442b884a
Igordanilo03 <141951957+Igordanilo03@users.noreply.github.com>
2026-01-26 18:43:33 -0300

    Fix: Config de emergencia com tipo any

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: any = {
   images: {
     remotePatterns: [
       {
@@ -13,7 +13,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  
   eslint: {
     ignoreDuringBuilds: true,
   },
