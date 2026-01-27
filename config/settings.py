16c606ed733c7d82711147f9629132235c1caff3
Igordanilo03 <141951957+Igordanilo03@users.noreply.github.com>
2026-01-26 21:17:58 -0300

    Fix: add md

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,6 @@ CORS_ALLOW_HEADERS = [
 
 # Application definition
 INSTALLED_APPS = [
-    'cloudinary_storage', # Deve vir antes de staticfiles
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
